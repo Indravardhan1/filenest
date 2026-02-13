4f6aa7c2a254ac98dae8744e1c768a4320605002
Indravardhan1 <indravardhanb012@gmail.com>
2026-02-13 11:45:16 +0530

    cleaned server static config

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
@@ -7,8 +8,13 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+
 app.use("/uploads", express.static("uploads"));
-app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "login.html"));
+});
 
 
 app.use("/api/auth", require("./routes/auth"));
